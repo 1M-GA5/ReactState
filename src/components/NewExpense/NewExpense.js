f3b998598f6175979aaf574175e606ff154a9bf9
1M-GA5 <athrvgajbhiyexxi@gmail.com>
2023-03-15 15:43:38 +0530

    First Commit

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewExpense.css'
+import ExpesnseForm from './ExpenseForm';
+
+const NewExpense = () =>{
+    return <div className='new-expense'>
+      <ExpesnseForm />
+    </div>
+}
+
+
+export default NewExpense;
